c483fa83755e7027575106945087ac6478b020bc
jamallasomasekhar <somasekharjamalla@gmail.com>
2026-03-27 17:52:09 +0530

    adding ci latest

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "🚀 Packaging Lambda..."
+echo "packing Lambda..."
 
-# Go to project root (safe execution)
+# Go to project root 
 ROOT_DIR=$(cd "$(dirname "$0")/.." && pwd)
 
-# Clean previous build
+# Clean old build
 rm -rf "$ROOT_DIR/scripts/build"
 mkdir -p "$ROOT_DIR/scripts/build"
 
-# Copy app code
+# Copy 
 cp "$ROOT_DIR/app/lambda_function.py" "$ROOT_DIR/scripts/build/"
-
-# (Optional) Install dependencies if requirements.txt exists
-if [ -f "$ROOT_DIR/app/requirements.txt" ]; then
-  echo "📦 Installing dependencies..."
-  pip install -r "$ROOT_DIR/app/requirements.txt" -t "$ROOT_DIR/scripts/build/"
-fi
 
 # Create zip
 cd "$ROOT_DIR/scripts/build"
 zip -r function.zip .
 
-echo "✅ Lambda package created at scripts/build/function.zip"
+echo "Lambda package created at scripts/build/function.zip"
